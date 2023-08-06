fb290fc6d0ae8b0c3e832f63862bc58d0af1dd6c
Madhab Sharma <madhabsharma94@gmail.com>
2023-08-06 20:42:51 +0530

    sys-monitor: Add progress bar
    Return progress bar as string from a ratio.

--- src/display_man.c
+++ src/display_man.c
@@ -1,19 +1,26 @@
 #include<ncurses.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "display_man.h"
 #include "system_info.h"
 
 
+static void display_generate_progress_bar(int max_len, int end_len, char **pb, float ratio);
+    
 /* display_top 
  * writes texts to the top window of the screen
  */
-void display_top(WINDOW *win, sys_info_t *sys_info) {
+void display_top(WINDOW *win, sys_info_t *sys_info, char **mem_pb, char **cpu_pb) {
 	int row = 0;
 	mvwprintw(win, ++row, 2, "OS		   : %s", sys_info->os);
 	mvwprintw(win, ++row, 2, "Kernel	   : %s", sys_info->kernel);
 	mvwprintw(win, ++row, 2, "Uptime	   : HH:%02ld-MM:%02ld-SS:%02ld", (sys_info->uptime % 86400)/3600, (sys_info->uptime%3600)/ 60, (sys_info->uptime)%60);
-	mvwprintw(win, ++row, 2, "CPU		   :");
+	display_generate_progress_bar(50, 10, mem_pb, 0.2);
+	display_generate_progress_bar(50, 10, cpu_pb, 0.2);
+    mvwprintw(win, ++row, 2, "CPU		   :");
+    wprintw(win, "%s", (*cpu_pb));
 	mvwprintw(win, ++row, 2, "MEM		   :");
+    wprintw(win, "%s", (*mem_pb));
 	mvwprintw(win, ++row, 2, "Total Processes  : %lld", sys_info->procs_info.total_processes);
 	mvwprintw(win, ++row, 2, "Running Processes: %lld", sys_info->procs_info.running_processes);
 	wrefresh(win);
@@ -25,6 +32,10 @@ void display_top(WINDOW *win, sys_info_t *sys_info) {
  * preodically.
  */
 void display_sys_win( sys_info_t *sys_info) {
+    char *mem_pb = NULL;
+    char *cpu_pb = NULL;
+
+    
 	int wmax_x = 0;
 	initscr();
 	noecho();
@@ -35,15 +46,42 @@ void display_sys_win( sys_info_t *sys_info) {
 	WINDOW *top_win = newwin(9, wmax_x -1, 0, 0);
 	
 	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    mem_pb = calloc(50 + 10, sizeof(char));
+    cpu_pb = calloc(50 + 10, sizeof(char));
 
 	while(1) {
 		box(top_win, 0, 0);
         system_get_time(sys_info);
         system_get_processes_info(sys_info);
-		display_top(top_win, sys_info);
+		display_top(top_win, sys_info, &mem_pb, &cpu_pb);
 		wrefresh(top_win);
 		refresh();
 		sleep(1);
 	}
 	endwin();
+    
+    free(mem_pb);
+    free(cpu_pb);
+    mem_pb = NULL;
+    cpu_pb = NULL;
+}
+
+/*
+ * Generate progress bar returns a string of progress bar
+ * for the ratio passed
+ */
+static void display_generate_progress_bar(int max_len, int end_len, char **pb, float ratio){
+
+    if(!pb || !*pb) {
+        fprintf(stderr, "Invalid pb passed!\n");
+        return;
+    }
+
+    int bars_count = ratio * max_len;
+    
+    for( int i = 0; i < max_len - 1; i++ )
+       (*pb)[i] = (i < bars_count)?'|':' ';
+
+    sprintf((*pb) + max_len - 1, "%02d/100%s", (int)(ratio * 100), "%");
+    
 }
